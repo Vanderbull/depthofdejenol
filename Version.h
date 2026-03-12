7eef34f7a1131b8e31dedfe8dba3559b53d3df50
rixxster <1743820+rixxster@users.noreply.github.com>
2026-03-12 04:40:00 +0100

    (fix): Build Artifacts Committed to Git

--- Version.h
+++ Version.h
@@ -3,10 +3,10 @@
 
 namespace GameConstants {
     // These will now correctly expand to value because of the quotes in the .pro
-    static const char* const GIT_HASH = "3d02a6e";
-    static const char* const BUILD_TIMESTAMP = "2026-03-07_22:35:58";
-    static const char* const FULL_VERSION = "v529"
-"3d02a6e";
+    static const char* const GIT_HASH = "13cd872";
+    static const char* const BUILD_TIMESTAMP = "2026-03-12_04:39:04";
+    static const char* const FULL_VERSION = "v541"
+"13cd872";
 }
 
 #endif
