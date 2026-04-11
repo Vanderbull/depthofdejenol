7d98731c98831a3caff5f65f8996dbd9904a05d2
rixxster <1743820+rixxster@users.noreply.github.com>
2026-04-11 11:01:57 +0200

    refactor
    rebuild whole code to make it simpler

--- src/library_dialog/library_dialog.h
+++ src/library_dialog/library_dialog.h
@@ -1,43 +1,30 @@
 #ifndef LIBRARY_DIALOG_H
 #define LIBRARY_DIALOG_H
 
-#include "src/core/GameConstants.h"
 #include <QDialog>
 #include <QMap>
-#include <QString>
 #include <QListWidget>
 #include <QTextEdit>
 #include <QComboBox>
 #include <QLabel>
-#include <QPushButton>
 #include <QLineEdit>
-#include "gameStateManager.h"
 
-class LibraryDialog : public QDialog
-{
+class LibraryDialog : public QDialog {
     Q_OBJECT
 public:
     explicit LibraryDialog(QWidget *parent = nullptr);
-private slots:
-    void onItemSelected(QListWidgetItem *item);
-    void onCloseClicked();
-    void onCategoryChanged(const QString &categoryName);
-    void onSearchTextChanged(const QString &searchText);
-    void onAddItemClicked();
+
 private:
     void setupUI();
     void loadKnowledge();
-    void updateList(const QString &category);
-    void updateCountLabel();
+    void refreshDisplay(); // Centralized update logic
+
     QMap<QString, QMap<QString, QString>> knowledgeBase;
     QListWidget *bookList;
     QTextEdit *descriptionText;
-    QComboBox *categoryComboBox;
-    QLabel *headerLabel;
-    QPushButton *addItemButton;
-    QPushButton *closeButton;
-    QLabel *countLabel;
+    QComboBox *categoryBox;
     QLineEdit *searchField;
+    QLabel *countLabel;
 };
 
-#endif // LIBRARY_DIALOG_H
+#endif
